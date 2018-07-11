327c252b7e618af9e2ec3a12a1d843954923f46b
Shane Sveller <shanesveller@gmail.com>
2018-07-10 20:32:25 -0500

    Bump project version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AppupPlayground.MixProject do
   def project do
     [
       app: :appup_playground,
-      version: "0.2.1",
+      version: "0.3.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
